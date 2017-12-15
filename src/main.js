d929c56f86b2d232a7e8e7c27d30703154faf694
Erdogan Bulut <ebulut@protel.com.tr>
2017-12-15 13:53:22 +0300

    Add and base config vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+
+// Vuex + Store
+import store from './store';
+
 import App from './App';
 import router from './router';
 
@@ -10,6 +14,7 @@ Vue.config.productionTip = false;
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App },
 });
