63e5bd757362a1b2933a93e235c533245c8c1b3f
Erdogan Bulut <ebulut@protel.com.tr>
2017-12-18 09:33:55 +0300

    Create headerbar component for example to shared component create

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
+    <header-bar />
     <router-view/>
   </div>
 </template>
 
 <script>
+import HeaderBar from './components/shareds/HeaderBar';
+
 export default {
   name: 'app',
+  components: {
+    HeaderBar,
+  },
 };
 </script>
 
---
+++ src/components/shareds/HeaderBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <h1>Vue Starter Kit</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderBar',
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+header {
+  height: 50px;
+  background: rgba(25, 75, 95, 1);
+  padding: 0 10px;
+  h1 {
+    margin: 0;
+    padding: 0;
+    color: #fff;
+    font-weight: 100;
+    font-size: 1.5rem;
+    line-height: 50px;
+  }
+}
+</style>
